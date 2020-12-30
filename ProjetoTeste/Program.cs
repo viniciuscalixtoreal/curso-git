c25bb9b1620a7006dbe09b4f8bf6edace7543b53
Vinicius Calixto Real <viniciuscalixtoreal@gmail.com>
2020-12-30 17:42:44 -0300

    Acrescentada a pergunta sobre como voce esta

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -8,6 +8,7 @@ namespace ProjetoTeste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
         }
     }
 }
