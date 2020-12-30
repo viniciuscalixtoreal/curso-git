7d4ebd8d3a2a689b841a0090a31636b5ba33f28b
Vinicius Calixto Real <viniciuscalixtoreal@gmail.com>
2020-12-30 17:24:35 -0300

    Acrescentada uma mensagem de bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
